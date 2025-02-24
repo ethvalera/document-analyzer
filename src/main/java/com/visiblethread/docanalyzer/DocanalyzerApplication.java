01f3a8583be1a03bc297a175fe38a6f0d93425e7
Elisabeth <ethvalera@gmail.com>
2025-02-24 22:41:59 +0000

    Initial commit for Document Analyzer project

---
+++ src/main/java/com/visiblethread/docanalyzer/DocanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.visiblethread.docanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocanalyzerApplication.class, args);
+	}
+
+}
